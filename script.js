b00c0abdda95964070e0274099c6a791c609da28
Lucas <lucasfernandomacedo13@gmail.com>
2022-04-22 19:30:59 -0300

    Requisitos do 7 ao 9

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable max-lines-per-function */
 const buttonAdd = document.querySelector('#criar-tarefa');
 const inputTask = document.querySelector('#texto-tarefa');
 const ol = document.querySelector('#lista-tarefas');
 
+// Adicionar uma tarefa na lista
 buttonAdd.addEventListener('click', () => {
   const li = document.createElement('li');
   li.innerText = inputTask.value;
+  li.classList.add('tarefa');
   ol.appendChild(li);
   inputTask.value = '';
-})
+  const listaDeTarefas = document.querySelectorAll('.tarefa');
+
+  // Selecionando as tarefas
+  for (let i = 0; i < listaDeTarefas.length; i += 1) {
+    listaDeTarefas[i].addEventListener('click', changeColor);
+  }
+  function changeColor(event) {
+    for (let i = 0; i < listaDeTarefas.length; i += 1) {
+      listaDeTarefas[i].classList.remove('active');
+    }
+    event.target.classList.add('active');
+  }
+
+  // Marcando as tarefas como concluídas
+  for (let i = 0; i < listaDeTarefas.length; i += 1) {
+    let activated = true;
+    listaDeTarefas[i].addEventListener('dblclick', (event) => {
+      event.target.className = activated ? 'completed' : '';
+      activated = !activated;
+    });
+  }
+});
