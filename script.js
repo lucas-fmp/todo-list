0ec43c64b6d3cc1c9ad0a05dee29f296eb9857eb
Lucas <lucasfernandomacedo13@gmail.com>
2022-04-25 21:16:57 -0300

    Requisito 11 e inicio do 12

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/* eslint-disable no-shadow */
+/* eslint-disable sonarjs/no-identical-functions */
+/* eslint-disable no-inner-declarations */
+/* eslint-disable complexity */
+/* eslint-disable no-param-reassign */
+/* eslint-disable sonarjs/cognitive-complexity */
 /* eslint-disable no-use-before-define */
 /* eslint-disable max-lines-per-function */
 const buttonAdd = document.querySelector('#criar-tarefa');
@@ -11,23 +17,21 @@ buttonAdd.addEventListener('click', () => {
   li.classList.add('tarefa');
   ol.appendChild(li);
   inputTask.value = '';
-  const listaDeTarefas = document.querySelectorAll('.tarefa');
-
   // Selecionando as tarefas
-  for (let i = 0; i < listaDeTarefas.length; i += 1) {
-    listaDeTarefas[i].addEventListener('click', changeColor);
+  for (let i = 0; i < ol.childNodes.length; i += 1) {
+    ol.childNodes[i].addEventListener('click', changeColor);
   }
   function changeColor(event) {
-    for (let i = 0; i < listaDeTarefas.length; i += 1) {
-      listaDeTarefas[i].classList.remove('active');
+    for (let i = 0; i < ol.childNodes.length; i += 1) {
+      ol.childNodes[i].classList.remove('active');
     }
     event.target.classList.add('active');
   }
 
   // Marcando as tarefas como concluídas
-  for (let i = 0; i < listaDeTarefas.length; i += 1) {
+  for (let i = 0; i < ol.childNodes.length; i += 1) {
     let activated = true;
-    listaDeTarefas[i].addEventListener('dblclick', (event) => {
+    ol.childNodes[i].addEventListener('dblclick', (event) => {
       event.target.className = activated ? 'completed' : '';
       activated = !activated;
     });
@@ -56,3 +60,67 @@ buttonClearCompleted.addEventListener('click', () => {
     }
   }
 });
+
+// Botão de salvar a lista
+const buttonSaveList = document.querySelector('#salvar-tarefas');
+const arrayClass = [];
+const arrayText = [];
+buttonSaveList.addEventListener('click', () => {
+  for (let i = 0; i < ol.childNodes.length; i += 1) {
+    arrayClass.push(ol.childNodes[i].className);
+    arrayText.push(ol.childNodes[i].innerText);
+  }
+  localStorage.setItem('class', JSON.stringify(arrayClass));
+  localStorage.setItem('text', JSON.stringify(arrayText));
+});
+
+// Renderização inicial
+function initialRenderization() {
+  if (localStorage.getItem('class') === null && localStorage.getItem('text') === null) {
+    localStorage.setItem('class', JSON.stringify([]));
+    localStorage.setItem('text', JSON.stringify([]));
+  } else {
+    for (let i = 0; i < JSON.parse(localStorage.getItem('class')).length; i += 1) {
+      const stringText = localStorage.getItem('text');
+      const newArrayText = JSON.parse(stringText);
+      const stringClass = localStorage.getItem('class');
+      const newArrayClass = JSON.parse(stringClass);
+      const li = document.createElement('li');
+      li.innerText = newArrayText[i];
+      li.className = `tarefa ${newArrayClass[i]}`;
+      ol.appendChild(li);
+      // Selecionando as tarefas
+      for (let i = 0; i < ol.childNodes.length; i += 1) {
+        ol.childNodes[i].addEventListener('click', changeColor);
+      }
+      function changeColor(event) {
+        for (let i = 0; i < ol.childNodes.length; i += 1) {
+          ol.childNodes[i].classList.remove('active');
+        }
+        event.target.classList.add('active');
+      }
+
+      // Marcando as tarefas como concluídas
+      for (let i = 0; i < ol.childNodes.length; i += 1) {
+        let activated = true;
+        ol.childNodes[i].addEventListener('dblclick', (event) => {
+          event.target.className = activated ? 'completed' : '';
+          activated = !activated;
+        });
+      }
+    }
+  }
+}
+
+// Botão de mover pra cima
+const buttonUp = document.querySelector('#mover-cima');
+buttonUp.addEventListener('click', () => {
+  const selected = ol.childNodes;
+  console.log(selected[0]);
+});
+
+// Botão de mover pra baixo
+
+window.onload = function () {
+  initialRenderization();
+};
